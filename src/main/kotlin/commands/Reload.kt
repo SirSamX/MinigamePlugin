f59490f6190e6531c3b2eed3f5d2e9b1eedcf997
SirSam <samuelheil@web.de>
2023-10-15 17:28:49 +0200

    Executable from console

--- src/main/kotlin/commands/Reload.kt
+++ src/main/kotlin/commands/Reload.kt
@@ -8,12 +8,9 @@ import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.command.TabCompleter
-import org.bukkit.entity.Player
 
 class Reload : CommandExecutor, TabCompleter {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
-        if (sender !is Player) return true
-
         Utils.broadcast(Component.text("RELOADING!", NamedTextColor.RED).decorate(TextDecoration.BOLD))
         sender.server.reload()
         Utils.broadcast(Component.text("RELOAD COMPLETE!", NamedTextColor.GREEN).decorate(TextDecoration.BOLD))
